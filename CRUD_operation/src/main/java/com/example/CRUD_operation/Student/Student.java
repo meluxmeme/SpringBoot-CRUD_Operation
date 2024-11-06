15410828e609e791dd10aa36fda7bbe4dabf0501
Laxmi Paudel <meluxmeme@gmail.com>
2024-11-05 19:29:29 -0700

    database connection is successful

--- CRUD_operation/src/main/java/com/example/CRUD_operation/Student/Student.java
+++ CRUD_operation/src/main/java/com/example/CRUD_operation/Student/Student.java
@@ -2,7 +2,25 @@ package com.example.CRUD_operation.Student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+    @Id@SequenceGenerator(
+        name = "student_sequence",
+        sequenceName= "student_sequence",
+        allocationSize =1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private int id;
     private String name;
     private String email;
@@ -19,7 +37,6 @@ public class Student {
 
     public Student() {
     }
-
 
     public int getId() {
         return id;
